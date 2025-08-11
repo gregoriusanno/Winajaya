6b0a9460d4f296d0aaeae0804519145e695add84
Dimas <sebastianus.anjangasmara@binus.ac.id>
2025-08-11 18:57:31 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,9 +7,9 @@ const path = require("path");
 const sequelize = require("./src/config/database");
 const routes = require("./src/routes");
 const allowedOrigins = [
-  "http://192.168.18.51:5173",
-  "http://localhost:5173",
-  "http://192.168.18.51:3002",
+  "http://192.168.20.124:5173",
+  "http://localhost:5174",
+  "http://192.168.20.124:3002",
   "capacitor://localhost",
   "http://localhost",
 ];
