31339dec453e5e748630c38c9088eeaa2a394c8a
Dimas <sebastianus.anjangasmara@binus.ac.id>
2025-08-24 16:44:11 +0700

    updateOrigin

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ const app = express();
 // middleware
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
