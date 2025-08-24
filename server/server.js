06e1a96bb03c6ab50898a4c9a162c2d2d2838a3b
Dimas <sebastianus.anjangasmara@binus.ac.id>
2025-08-24 17:05:32 +0700

    u[date

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
-const express = require("express");
 const cors = require("cors");
+const express = require("express");
 const path = require("path");
 
 const sequelize = require("./src/config/database");
